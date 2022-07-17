d696c8eae577c26e747d8e77f6659dee490e647f
CherifiMi <cherifi154949@gmail.com>
2022-07-17 01:52:27 +0100

    color changing using coroutines

--- app/src/main/java/com/example/learning_android/MainActivity.kt
+++ app/src/main/java/com/example/learning_android/MainActivity.kt
@@ -1,26 +1,16 @@
 package com.example.learning_android
 
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.learning_android.ui.theme.Learning_AndroidTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
 
@@ -30,32 +20,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Learning_AndroidTheme {
-                Surface(modifier = Modifier.background(Color(color.value))) {
-
-                }
+                Box(modifier = Modifier.background(Color(color.value)).fillMaxSize())
             }
         }
 
-        //________________________________Coroutines
-
-        GlobalScope.launch(Dispatchers.IO) {
-            val netAnswar = doNetCall()
-            val netAnswar2 = doNetCall2()
-            Log.d("HILLOCORE", netAnswar)
-            Log.d("HILLOCORE", netAnswar2)
+        GlobalScope.launch(Dispatchers.Main) {
+            for (i in 1..100000000){
+                color.value = changeColor(color.value)
+                delay(10)
+            }
         }
 
     }
 
+    fun changeColor(value: Long): Long {
 
+        return value-1
 
-    suspend fun doNetCall(): String {
-        delay(3000L)
-        return "This is Data"
     }
-    suspend fun doNetCall2(): String {
-        delay(3000L)
-        return "This is Data 2"
-    }
+
 }
 
